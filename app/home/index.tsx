188f675133c07b89bb70567f272b15366745a4e7
hugobepa <hugo_bcn@hotmail.com>
2025-12-19 19:07:37 +0100

    appMovies_pantCarga_SafeArea

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,14 +1,23 @@
 import { useMovies } from "@/presentation/hooks/useMovies";
 import React from "react";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const safeArea = useSafeAreaInsets();
   const { nowPlayingQuery } = useMovies();
 
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className="justify-center items-center flex-1">
+        <ActivityIndicator color="purple" size={30} />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Text>{JSON.stringify(nowPlayingQuery.data, null, 2)}</Text>
+    <View className="mt-2" style={{ paddingTop: safeArea.top }}>
+      <Text className="text-3xl font-bold px-4 mb-2">HomeScreen</Text>
     </View>
   );
 };
--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -2,22 +2,15 @@ import { movieApi } from "@/core/api/movie-api";
 import { MovieDBMoviesResponse } from "@/infrastructure/interfaces/moviedb-response";
 import { MovieMapper } from "@/infrastructure/mappers/movie.mappers";
 
-
 export const nowPlayingAction = async () => {
+  try {
+    const { data } = await movieApi.get<MovieDBMoviesResponse>("/now_playing");
 
-    try {
-        
-        const {data} = await movieApi.get<MovieDBMoviesResponse>('/now_playing')
-       
-        const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie)
-        
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
 
-        console.log(JSON.stringify(movies,null,2))
-        return movies;
-
-    } catch (error) {
-        console.log({error})
-        throw 'Cannot load now playing movies'
-    }
-
-}
+    return movies;
+  } catch (error) {
+    console.log({ error });
+    throw "Cannot load now playing movies";
+  }
+};
