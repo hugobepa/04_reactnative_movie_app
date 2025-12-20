fd4331018fa6ebfca6e804b41df5323025377708
hugobepa <hugo_bcn@hotmail.com>
2025-12-20 10:30:00 +0100

    appMovies_finScrollFlatList

--- presentation/components/movies/MovieHorizontalList2.tsx
+++ presentation/components/movies/MovieHorizontalList2.tsx
@@ -1,14 +1,47 @@
 import { Movie } from "@/infrastructure/interfaces/movie.interface";
-import { FlatList, Text, View } from "react-native";
+import { useRef } from "react";
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Text,
+  View,
+} from "react-native";
 import MoviePoster from "./MoviePoster";
 
 interface Props {
   title?: string;
   movies: Movie[];
   className?: string;
+
+  loadNextPage?: () => void;
 }
 
-const MovieHorizontalList2 = ({ title, movies, className }: Props) => {
+const MovieHorizontalList2 = ({
+  title,
+  movies,
+  className,
+  loadNextPage,
+}: Props) => {
+  const isLoading = useRef(false);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return;
+
+    const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+
+    const isEndReached =
+      contentOffset.x + layoutMeasurement.width + 600 >= contentSize.width;
+
+    if (!isEndReached) return;
+
+    isLoading.current = true;
+
+    //TODO:
+    console.log("cargar siguientes peliculas");
+    loadNextPage && loadNextPage();
+  };
+
   return (
     <View className={` ${className}`}>
       {title && <Text className="text-3xl font-bold px-4 mb-2">{title}</Text>}
@@ -21,6 +54,7 @@ const MovieHorizontalList2 = ({ title, movies, className }: Props) => {
         renderItem={({ item }) => (
           <MoviePoster id={item.id} poster={item.poster} smallPoster />
         )}
+        onScroll={onScroll}
       />
     </View>
   );
