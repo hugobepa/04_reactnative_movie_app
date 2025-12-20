c563cddd12637bc11be595e1736fb43d72ad40b1
hugobepa <hugo_bcn@hotmail.com>
2025-12-20 17:23:07 +0100

    AppMovie_DescripPeli

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,3 +1,4 @@
+import MovieDescription from "@/presentation/components/movie/MovieDescription";
 import MovieHeader from "@/presentation/components/movie/MovieHeader";
 import { useMovie } from "@/presentation/hooks/useMovie";
 import { useLocalSearchParams } from "expo-router";
@@ -25,6 +26,8 @@ const MovieScreen = () => {
         originalTitle={movieQuery.data.originalTitle}
         title={movieQuery.data.title}
       />
+
+      <MovieDescription movie={movieQuery.data} />
     </ScrollView>
   );
 };
