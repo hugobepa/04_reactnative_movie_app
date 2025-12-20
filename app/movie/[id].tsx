aabef68e6e17a363c16cc85e641ad13be5ba765e
hugobepa <hugo_bcn@hotmail.com>
2025-12-20 20:05:35 +0100

    AppMovie_addCast

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,3 +1,4 @@
+import MovieCast from "@/presentation/components/movie/MovieCast";
 import MovieDescription from "@/presentation/components/movie/MovieDescription";
 import MovieHeader from "@/presentation/components/movie/MovieHeader";
 import { useMovie } from "@/presentation/hooks/useMovie";
@@ -8,7 +9,7 @@ import { ActivityIndicator, ScrollView, Text, View } from "react-native";
 const MovieScreen = () => {
   const { id } = useLocalSearchParams();
 
-  const { movieQuery } = useMovie(+id);
+  const { movieQuery, castQuery } = useMovie(+id);
 
   if (movieQuery.isLoading || !movieQuery.data) {
     return (
@@ -28,6 +29,7 @@ const MovieScreen = () => {
       />
 
       <MovieDescription movie={movieQuery.data} />
+      <MovieCast cast={castQuery.data ?? []} />
     </ScrollView>
   );
 };
